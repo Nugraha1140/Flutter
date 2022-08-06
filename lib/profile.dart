078118bd4ed13d1d0d37c8e8871470ab0e7dcc3a
nugraha1140 <nugrahaadi247@gmail.com>
2022-08-06 11:46:17 +0700

    Flutter Next Part

--- lib/profile.dart
+++ lib/profile.dart
@@ -17,8 +17,8 @@ class Profile extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center ,
             children: [
                       Container(
-                        width: 200,
-                        height: 60,
+                        width: 320,
+                        height: 50,
                         margin: EdgeInsets.all(10),
                         decoration:  BoxDecoration(
                           gradient: LinearGradient(colors: [Colors.blueAccent, Colors.redAccent]),
@@ -129,19 +129,19 @@ class Profile extends StatelessWidget {
                         ],
                       ),
                        Container(
-                        width: 315,
-                        height: 90,
+                        width: 320,
+                        height: 100,
                         margin: EdgeInsets.all(10),
                         decoration:  BoxDecoration(
                           gradient: LinearGradient(colors: [Colors.blueAccent, Colors.redAccent]),
                           borderRadius:  BorderRadius.circular(10)),
                           child: Center(
                             child: Text(
-                              "Ini Namanya Patrick Star, Ia Adalah Salah Satu Tokoh Dalam Serial Kartun Spongebob. Patrick Adalah Karakter Yang Sedikit Ceroboh namun Terkadang bijak, Patrick Seringkali Melontarkan Atau Melakukan tindakan yang tidak biasa",
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                               style: TextStyle(
                                 color: Colors.white,
                                 fontFamily: 'DancingScript',
-                                fontSize: 15
+                                fontSize: 12,
                               ),
                             ),
                           ),
@@ -192,8 +192,8 @@ class Profile extends StatelessWidget {
                       ],
                       ),
                        Container(
-                        width: 300,
-                        height: 60,
+                        width: 320,
+                        height: 50,
                         margin: EdgeInsets.all(10),
                         decoration:  BoxDecoration(
                           gradient: LinearGradient(colors: [Colors.blueAccent, Colors.redAccent]),
